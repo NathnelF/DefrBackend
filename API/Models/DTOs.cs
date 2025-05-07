9ef7b357b10ceef4d31297a2af6d9f439b3f242b
NathnelF <136366777+NathnelF@users.noreply.github.com>
2025-05-07 11:36:59 -0400

    Added Dtos for the rest of the relevant tables.

--- API/Models/DTOs.cs
+++ API/Models/DTOs.cs
@@ -9,3 +9,39 @@ public class ServiceDto
 {
     public string? Name { get; set; }
 }
+
+public class ContractDto
+{
+    public int ServiceId { get; set; }
+
+    public int CustomerId { get; set; }
+
+    public decimal Price { get; set; }
+
+    public DateTime? OriginalContractStart { get; set; }
+
+    public DateTime? CurrentTermStart { get; set; }
+
+    public DateTime? CurrentTermEnd { get; set; }
+
+    public int? TermLength { get; set; }
+
+    public bool? IsAutoRenew { get; set; }
+
+    public decimal? RenewalPriceIncrease { get; set; }
+
+    public bool? IsChurned { get; set; }
+}
+
+public class RecognitionEventDto
+{
+    public int Id { get; set; }
+
+    public int ServiceId { get; set; }
+
+    public int CustomerId { get; set; }
+
+    public int ContractId { get; set; }
+
+    public decimal? Amount { get; set; }
+}

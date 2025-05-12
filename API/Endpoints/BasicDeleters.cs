244946ef444b4a97867fa971a12cb0002eea9403
NathnelF <136366777+NathnelF@users.noreply.github.com>
2025-05-12 10:58:22 -0400

    Added delete contract by Id

--- API/Endpoints/BasicDeleters.cs
+++ API/Endpoints/BasicDeleters.cs
@@ -9,13 +9,17 @@ public static class BasicDeleters
     public static void MapBasicDeleters(this WebApplication app)
     {
         app.MapDelete("/clear_schedules", ClearScheduleById);
+        app.MapDelete("/delete_contract", DeleteContractById);
     }
 
     public static async Task<IResult> ClearScheduleById(MyContext db, int contractId)
     {
         await db.RecognitionEvents.Where(e => e.ContractId == contractId).ExecuteDeleteAsync();
         return Results.Ok($"Cleared schedule for contract: {contractId}");
-
-
+    }
+    public static async Task<IResult> DeleteContractById(MyContext db, int contractId)
+    {
+        await db.Contracts.Where(c => c.Id == contractId).ExecuteDeleteAsync();
+        return Results.Ok($"Cleared contract: {contractId}");
     }
 }
